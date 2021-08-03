293f8af251618070f00631dc5c09cdf2b0bfed1a
dkai <D.Kainar@timir.kz>
2021-08-03 14:37:16 +0600

    добавил меню(2) module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminPanelComponent
+    AdminPanelComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
