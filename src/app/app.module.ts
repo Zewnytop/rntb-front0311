d6d5a726d4319d938de80ab4e158b07db6db8aa9
dkai <D.Kainar@timir.kz>
2021-08-04 15:28:47 +0600

    Меню-навбар

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './menu-navbar/menu.component';
 
 @NgModule({
   declarations: [
