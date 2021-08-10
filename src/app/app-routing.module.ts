b2da077524143f9180c205fc5a40f0430b0ec76b
nkoz <N.Kozhabayev@timir.kz>
2021-08-10 13:29:42 +0600

    Новый компонент и изменения роутера

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import {FooterComponent} from "./footer/footer.component";
 import {MenuComponent} from "./menu-navbar/menu.component";
 import {AdminPanelComponent} from "./admin-panel/admin-panel.component";
 import {GeneralComponent} from "./general/general.component";
+import {ContactsComponent} from "./contacts/contacts.component";
 
 const routes: Routes = [
   {path: '', component: GeneralComponent},
   {path: 'menu-navbar', component: MenuComponent},
   {path: 'footer', component: FooterComponent},
+  {path: 'contacts', component: ContactsComponent},
   {path: 'admin-panel', component: AdminPanelComponent},
+
 ];
 
 const adminRoutes: Routes = [
