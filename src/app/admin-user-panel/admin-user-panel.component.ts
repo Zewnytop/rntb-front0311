76493f8fb871b543768e158a11d5e715849c8cc0
dkai <D.Kainar@timir.kz>
2021-10-18 11:33:58 +0600

    admin-user языки для отключения

--- src/app/admin-user-panel/admin-user-panel.component.ts
+++ src/app/admin-user-panel/admin-user-panel.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-user-panel.component.css']
 })
 export class AdminUserPanelComponent implements OnInit {
+  private _lang: string = "ru";
 
+  get lang(): string {
+    return this._lang;
+  }
+
+  set lang(value: string) {
+    this._lang = value;
+  }
   constructor() { }
 
   ngOnInit(): void {
