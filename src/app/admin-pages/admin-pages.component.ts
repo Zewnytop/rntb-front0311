0abca80c6e032efef1fa0122722299bb2b8e905d
dkai <D.Kainar@timir.kz>
2021-10-19 20:24:10 +0600

    pages tables

--- src/app/admin-pages/admin-pages.component.ts
+++ src/app/admin-pages/admin-pages.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AdminGalleryComponent} from "../admin-gallery/admin-gallery.component";
+import {ViewVirtualExhibitionObject} from "../../site-object/view-virtual-exhibition-object";
+import {VirtualExhibitionService} from "../service/virtual-exhibition.service";
+import {ViewContactObject} from "../../site-object/contact-object";
+import {ContactService} from "../service/contact.service";
+
 
 @Component({
   selector: 'app-admin-pages',
@@ -7,9 +13,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPagesComponent implements OnInit {
 
-  constructor() { }
+  private _listCategoryVirtualExhibition: ViewVirtualExhibitionObject[] = [];
+  private _listViewContactBranch: ViewContactObject[] = [];
+  private _lang: string = "ru";
+
+  get listCategoryVirtualExhibition(): ViewVirtualExhibitionObject[] {
+    return this._listCategoryVirtualExhibition;
+  }
+
+  set listCategoryVirtualExhibition(value: ViewVirtualExhibitionObject[]) {
+    this._listCategoryVirtualExhibition = value;
+  }
+
+  get listViewContactBranch(): ViewContactObject[] {
+    return this._listViewContactBranch;
+  }
+
+  set listViewContactBranch(value: ViewContactObject[]) {
+    this._listViewContactBranch = value;
+  }
+
+  get lang(): string {
+    return this._lang;
+  }
+
+  set lang(value: string) {
+    this._lang = value;
+  }
+
+  constructor(private virtualExhibitionService: VirtualExhibitionService,private contactService: ContactService) {
+  }
 
   ngOnInit(): void {
+    this.getCategories();
+    this.getContacts();
+  }
+
+  getCategories(): void {
+    this.virtualExhibitionService.getCategories(parseInt(localStorage.getItem("BranchId")!!)).subscribe(data => {
+      data.result.forEach(category => {
+        this.listCategoryVirtualExhibition.push({
+          id: category.id,
+          nameRu: category.nameRu,
+          nameEn: category.nameEn,
+          nameKz: category.nameKz,
+          showOnPage: category.showOnPage,
+          isEdit: false,
+          lastModifiedDate: category.lastModifiedDate,
+          typeComponent: category.typeComponent,
+          libraryBranch: category.libraryBranch
+        });
+      });
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getContacts(): void {
+    this.contactService.getListContact(parseInt(localStorage.getItem("BranchId")!!)).subscribe(data => {
+      data.result.forEach((value) => this.listViewContactBranch.push({
+        id: value.id,
+        name: value.name,
+        lastModifiedDate: value.lastModifiedDate,
+        mainContact: value.mainContact
+      }));
+    }, error => {
+      console.log(error)
+    });
   }
 
 }
