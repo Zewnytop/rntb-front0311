f5a52e522622f285660a9111f781ae479146e4d6
Islam <I.Azizov@timir.kz>
2021-10-19 19:53:05 +0600

    на скорую руку

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {MenuObject} from "../../site-object/menu-object";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-footer',
@@ -7,19 +9,74 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  private _listMainItemMenu: any[] = [];
+
+  get listMainItemMenu(): any[] {
+    return this._listMainItemMenu;
+  }
+
+  set listMainItemMenu(value: any[]) {
+    this._listMainItemMenu = value;
+  }
+
   status: boolean = false;
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
   }
 
   ngOnInit(): void {
+    this.getMainitemsMenu();
   }
 
-  changeStatus(): void {
-    if (this.status) {
-      this.status = false;
+  getMainitemsMenu(): void {
+    const urlWithSlash = document.baseURI.replace(/.*\/\//, '');
+    const baseURI = urlWithSlash.replace('/', '');
+    const url = `/api/page/menu/${baseURI}`;
+    this.httpClient.get<any>(url).subscribe(data => {
+      console.log(data);
+      this.listMainItemMenu = this.setListItemMenu(data.result);
+      console.log("dasdasdas");
+      console.log(this.listMainItemMenu);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  setListItemMenu(itemsMenu: [any]): any[] {
+    let listItemMenu: any[] = [];
+    itemsMenu.forEach((item) => {
+      let itemMenu: { [k: string]: any } = {
+        id: item.id,
+        showItem: item.showItem,
+        serialNumber: item.serialNumber,
+        nameRu: item.nameRu,
+        nameEn: item.nameEn,
+        nameKz: item.nameKz,
+        description: item.description,
+        lastModifiedDate: item.lastModifiedDate,
+        isEdit: false,
+        isOpen: false,
+        parentItem: item.parentItem,
+        file: item.file,
+        typeComponent: item.typeComponent,
+        libraryBranch: item.libraryBranch,
+        // childerItemMenu: MenuObject[],
+        typeItemMenu: item.typeItemMenu
+      };
+      if (item.childerItemMenu) {
+        itemMenu['childerItemMenu'] = this.setListItemMenu(item.childerItemMenu);
+      }
+      listItemMenu.push(itemMenu);
+    }, this);
+    return listItemMenu;
+  }
+
+
+  changeStatus(item: any): void {
+    if (item.isOpen) {
+      item.isOpen = false;
     } else {
-      this.status = true;
+      item.isOpen = true;
     }
   }
 
