46fb469b2891c49543d9a9d36ba20362b2979216
Islam <I.Azizov@timir.kz>
2021-10-26 16:34:43 +0600

    Сборка сайта контакты

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {SiteContactObject} from "../../site-object/site-component-object";
+import {DomSanitizer} from "@angular/platform-browser";
+import {SitePageService} from "../service/site-page.service";
 
 @Component({
   selector: 'app-contacts',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
-  prmn = "<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11628.112992163966!2d76.92747116088864!3d43.22986739302978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38836f20c57dc4ff%3A0xd15d1f5c219478e3!2z0JPQvtGA0L7QtNGB0LrQvtC1INGD0L_RgNCw0LLQu9C10L3QuNC1INC00L7RgNC-0LbQvdC-0Lkg0L_QvtC70LjRhtC40Lg!5e0!3m2!1sru!2skz!4v1634704508239!5m2!1sru!2skz\" width=\"600\" height=\"450\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\"></iframe>";
-  constructor() { }
 
-  ngOnInit(): void {
+  private _id: number | null = null;
+  private _branchContact: SiteContactObject | null = null;
+
+  get id(): number | null {
+    return this._id;
   }
 
+  @Input()
+  set id(value: number | null) {
+    this._id = value;
+  }
+
+  get branchContact(): SiteContactObject | null {
+    return this._branchContact;
+  }
+
+  set branchContact(value: SiteContactObject | null) {
+    this._branchContact = value;
+  }
+
+  constructor(private sitePageService: SitePageService, private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit(): void {
+    this.getContact();
+  }
+
+  getContact(): void {
+    this.sitePageService.getContact(this.id, "ru").subscribe(data => {
+      this.branchContact = data.result;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getMapSrc(srcMap: string): any {
+      return this.sanitizer.bypassSecurityTrustResourceUrl(srcMap);
+  }
 }
